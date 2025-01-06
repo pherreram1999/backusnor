5a93ea6545722df819bda3d3ff4c7e6e42aca58d
Pedro Alonso Herrera Mauricio <alonso.pahm@gmail.com>
2025-01-06 02:13:56 -0600

    se tiene un version preliminar

--- main.go
+++ main.go
@@ -2,57 +2,61 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
 const (
 	IfsNum   = 10
-	MaxSteps = 1000
+	MaxSteps = 10
 )
 
 func main() {
 	stack := NewSymboStack()
 	stack.AddFromString("iCtSA")
 
-	var ifCounter uint = 0
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
 	move := "R"
-	stepCounter := 0
+	ifCounter := 0
 
-	for stack.Head != nil && ifCounter < IfsNum && stepCounter < MaxSteps {
-		stepCounter++
+	for stack.Head != nil && ifCounter < MaxSteps {
 
-		fmt.Printf("Estado: %s, Cabezal: %c, Movimiento: %s, Cinta: [%s]\n",
-			stack.State,
-			stack.Head.Symbol,
-			move,
-			stack.String(),
-		)
+		//fmt.Println("Movement:", move, "- Stack:", stack, "Symbol Input", stack.Head)
 
-		if stack.State == "q0" && stack.Head.Symbol == 'S' {
+		if stack.State == "q0" && stack.Head.Symbol == 'A' {
+			if r.Float64() > .50 || stack.Head.Right == nil {
+				stack.insertString(";eS")
+			} else {
+				stack.Remove(stack.Head)
+			}
+			if stack.Head != nil && stack.Head.Right == nil {
+				move = "L" // porque es el ultimo nodo
+				stack.State = "q1"
+			} else {
+				move = "R"
+			}
+		} else if stack.State == "q0" && stack.Head.Right == nil {
+			stack.State = "q1"
+			move = "L"
+		} else if stack.State == "q1" && stack.Head.Symbol == 'S' {
 			stack.insertString("iCtSA")
 			ifCounter++
-
 			stack.State = "q1"
-			move = "R"
-		} else if stack.State == "q1" && stack.Head.Symbol == 'A' {
-			stack.insertString(";eS")
-			stack.State = "q0"
 			move = "L"
+		} else if stack.State == "q1" && stack.Head.Left == nil {
+			stack.State = "q0"
+			move = "R"
 		}
 
-		// Movimiento del cabezal
 		if move == "R" {
 			stack.MoveRigth()
 		} else {
 			stack.MoveLeft()
 		}
 
-		time.Sleep(1 * time.Second)
 	}
 
-	if stepCounter >= MaxSteps {
-		fmt.Println("Máquina estancada. Terminando ejecución.")
-	} else {
-		fmt.Println("Ejecución terminada correctamente.")
-	}
+	fmt.Println(stack)
+
 }
